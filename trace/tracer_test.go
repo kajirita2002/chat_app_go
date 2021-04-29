915723714d81ac3ccc23c458a89a5bd577c4404e
rita <kajirita2002@gmail.com>
2021-04-29 16:24:47 +0900

    add traicer unit test

---
+++ trace/tracer_test.go
@@ -0,0 +1,22 @@
+package trace
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	// bytes.BufferはWriterメソッドを持っている
+	// bytes.Buffer
+	var buf bytes.Buffer
+	tracer := New(&buf)
+	if tracer == nil {
+		t.Error("Newからの戻り値がnilです")
+	} else {
+		tracer.Trace("こんにちは、traceパッケージ")
+		// bufに保持されているので検証してしてみる
+		if buf.String() != "こんにちは、traceパッケージ\n" {
+			t.Errorf("'%s'という誤った文字列が検出されました", buf.String())
+		}
+	}
+}
