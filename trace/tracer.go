75b2a0420fbb682c5ca5f8c5293f0d63cc79b663
rita <kajirita2002@gmail.com>
2021-04-29 16:26:44 +0900

    add tracer pacakge

--- trace/tracer.go
+++ trace/tracer.go
@@ -1,6 +1,24 @@
 package trace
 
+import (
+	"fmt"
+	"io"
+)
+
 type Tracer interface {
 	Trace(...interface{})
 }
 
+func New(w io.Writer) Tracer {
+	return &tracer{out: w}
+}
+
+type tracer struct {
+	out io.Writer
+}
+
+func (t *tracer) Trace(a ...interface{}) {
+	// 書き込みを行う(テストではBytes.buffers.Writeが行われている)
+	t.out.Write([]byte(fmt.Sprint(a...)))
+	t.out.Write([]byte("\n"))
+}
