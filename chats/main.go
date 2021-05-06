32763d59c1014c31e4a7688ce42da2f13a30cf57
rita <kajirita2002@gmail.com>
2021-05-06 15:29:45 +0900

    add some providers

--- chats/main.go
+++ chats/main.go
@@ -10,6 +10,10 @@ import (
 	"text/template"
 
 	"github.com/kaji2002/chat_app/trace"
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/facebook"
+	"github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/gomniauth/providers/google"
 )
 
 type templateHandler struct {
@@ -29,6 +33,12 @@ var host = flag.String("host", ":8080", "The host of the application")
 
 func main() {
 	flag.Parse()
+	gomniauth.SetSecurityKey("98dfbg7iu2nb4uywevihjw4tuiyub34noilk")
+	gomniauth.WithProviders(
+		facebook.New("2891035937832238", "dc17fa42bf71f058cd1c8246d3c701e4", "http://localhost:8080/auth/callback/facebook"),
+		github.New("4c86aced996c4c012f75", "258d77a40adf1367faec823baa08f0cd58f04228", "http://localhost:8080/auth/callback/github"),
+		google.New("http://441470423311-l3va9r1oo3mh0uqtpaehnclpqkb4m90n.apps.googleusercontent.com/", "cd6JZE8tagHAT_2NycTfGl8-", "http://localhost:8080/auth/callback/github"),
+	)
 	r := newRoom()
 	// os.Stoutでターミナルに出力が行われる
 	r.tracer = trace.New(os.Stdout)
