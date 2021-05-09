2c7ac62418c41e1a0405c8a87069b05673649fa1
rita <kajirita2002@gmail.com>
2021-05-10 01:23:17 +0900

    merge three avatar system

--- chats/avatar.go
+++ chats/avatar.go
@@ -18,6 +18,17 @@ type Avatar interface {
 	GetAvatarURL(ChatUser) (string, error)
 }
 
+type TryAvatars []Avatar
+
+func (a TryAvatars) GetAvatarURL(u ChatUser) (string, error) {
+	for _, avatar := range a {
+		if url, err := avatar.GetAvatarURL(u); err != nil {
+			return url, nil
+		}
+	}
+	return "", ErrNoAvatarURL
+}
+
 // 空の構造体として定義
 // フィールドがないのでメソッドはレシーバーを参照する必要がない
 type AuthAvatar struct{}
